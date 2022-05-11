ce17473fbad1dd28c61a69c9d400fa50f000ab8c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-11 15:36:33 +0600

    MongoDB connection completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,31 @@ const port = process.env.PORT || 5000;
 // Middlewire
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voerh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('db connected');
+        const servicesCollections = client.db('doctors_portal').collection('services');
+
+        app.get('/service', async(req, res)=> {
+            const query = {};
+            const cursor = servicesCollections.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello from doctors portal')
