970d6c04fceb48fcfa11bebae144e59f265514a2
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-13 21:54:12 +0600

    booking api created for save data in database

--- index.js
+++ index.js
@@ -14,21 +14,46 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voerh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         console.log('db connected');
         const servicesCollections = client.db('doctors_portal').collection('services');
+        const bookingCollections = client.db('doctors_portal').collection('bookings');
 
-        app.get('/service', async(req, res)=> {
+
+
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = servicesCollections.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
+        /**
+         * API Naming Convention
+         * app.get('/booking') // get all bookings in this collection
+         * app.get('/booking/:id') // get a specific booking 
+         * app.post('/booking') // add a new booking
+         * app.patch('/booking/:id') 
+         * app.delete('/booking/:id') 
+        */
+
+
+        // booking api
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollections.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
+            const result = await bookingCollections.insertOne(booking);
+            return res.send({ success: true, result });
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
