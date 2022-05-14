c9cb57730283a32a40b55d6f8eae35e7f309cb96
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-15 00:55:40 +0600

    token virification done

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.voerh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
+
 async function run() {
     try {
         await client.connect();
@@ -44,7 +60,7 @@ async function run() {
             };
             const result = await userCollections.updateOne(filter, updateDoc, options);
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-            res.send({result, token});
+            res.send({ result, token });
         })
 
         // Warning !!!!;
@@ -86,11 +102,17 @@ async function run() {
          * app.delete('/booking/:id') 
         */
 
-        app.get('/booking', async (req, res) => {
+        app.get('/booking', verifyJWT, async (req, res) => {
             const patient = req.query.patient;
-            const query = { patient: patient };
-            const bookings = await bookingCollections.find(query).toArray();
-            res.send(bookings);
+            const decodedEamil = req.decoded.email;
+            if (patient === decodedEamil) {
+                const query = { patient: patient };
+                const bookings = await bookingCollections.find(query).toArray();
+                return res.send(bookings);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden Access' })
+            }
 
         })
 
