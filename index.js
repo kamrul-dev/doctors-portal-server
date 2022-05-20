b91269ae20a32a7778aac0e3215e794f25d5615f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-05-20 15:54:02 +0600

    booking api by id created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -151,6 +151,14 @@ async function run() {
             }
 
         });
+
+        //booking api by id
+        app.get('/booking/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await bookingCollections.findOne(query);
+            res.send(booking);
+        })
 
         // doctor api
         app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
